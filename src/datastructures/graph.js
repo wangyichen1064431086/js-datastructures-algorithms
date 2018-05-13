0a6da6839369ae4033b93f8bac5935149376ea6c
wangyichen1064431086 <1064431086@qq.com>
2018-05-13 21:46:52 +0800

    learned Prim algorithums

--- src/datastructures/graph.js
+++ src/datastructures/graph.js
@@ -3,7 +3,7 @@ import Queue from './queue.js';
 import Stack from './stack.js';
 /***********图的js实现*************/
 
-function Graph() {
+function Graph() { //MARK: 基于邻接表
   const vertices = [];//存储所有顶点的名字
   const adjList = new Dictionary();//字典存储邻接表
 
@@ -204,7 +204,7 @@ graph.addEdge('D', 'H');
 graph.addEdge('B', 'E');
 graph.addEdge('B', 'F');
 graph.addEdge('E', 'I');
-
+console.log('graph instance:')
 console.log(graph.toString());
 
 function printNode(v) {
@@ -281,77 +281,148 @@ for (let toV of otherVertices) {
 
 
 
- //最短路径算法：Dijkstra算法
-   ///寻找顶点A和其余顶点之间的最短路径：
-  const graphNew = [ //A, B, C, D, E, F顶点的邻接矩阵
-       //A, B, C, D, E, F
-   /*A*/[0, 2, 4, 0, 0, 0],//A->B距离为2， B->A距离为0
-   /*B*/[0, 0, 1, 4, 2, 0],
-   /*C*/[0, 0, 0, 0, 3, 0],
-   /*D*/[0, 0, 0, 0, 0, 2],
-   /*E*/[0, 0, 0, 3, 0, 2],
-   /*F*/[0, 0, 0, 0, 0, 0]     
-  ];
-  const INF = Number.MAX_SAFE_INTEGER;//JavaScript最大数，该数是一个十进制16位的数
+//最短路径算法：
+///1.Dijkstra算法:基于邻接矩阵
+  ///寻找顶点A和其余顶点之间的最短路径：
+const graphNew = [ //A, B, C, D, E, F顶点的邻接矩阵
+      //A, B, C, D, E, F
+  /*A*/[0, 2, 4, 0, 0, 0],//A->B距离为2， B->A距离为0
+  /*B*/[0, 0, 1, 4, 2, 0],
+  /*C*/[0, 0, 0, 0, 3, 0],
+  /*D*/[0, 0, 0, 0, 0, 2],
+  /*E*/[0, 0, 0, 3, 0, 2],
+  /*F*/[0, 0, 0, 0, 0, 0]     
+];
+const INF = Number.MAX_SAFE_INTEGER;//JavaScript最大数，该数是一个十进制16位的数
 
-  const minDistance = function(dist, visited) {
-    /**
-     * @param dist  TYPE Array[Number],存放A分别到A、B、C、D、E、F的距离。在dijkstra中调用时的初始值为[0, INF, INF, INF, INF, INF]
-     * @param visited TYPE Array[Bool],存放是否已经访问过A、B、C、D、E、F。在dijkstra中调用时的初始值为[false, false, false, false, false, false]
-     * @description 搜索dist数组的最小值，并返回它在dist中的索引，即可以找到离A最近的那个点的索引
-     */
-    let min = INF;
-    let minIndex = -1;
+const minDistance = function(dist, visited) {
+  /**
+   * @param dist  TYPE Array[Number],存放A分别到A、B、C、D、E、F的距离。在dijkstra中调用时的初始值为[0, INF, INF, INF, INF, INF]
+   * @param visited TYPE Array[Bool],存放是否已经访问过A、B、C、D、E、F。在dijkstra中调用时的初始值为[false, false, false, false, false, false]
+   * @description 搜索dist数组的最小值，并返回它在dist中的索引，即可以找到离A最近的那个点的索引
+   */
+  let min = INF;
+  let minIndex = -1;
 
-    for(let v = 0; v < dist.length; v++) {
-      if (visited[v] === false && dist[v] <= min) {
-        min = dist[v];
-        minIndex = v;
-      }
+  for(let v = 0; v < dist.length; v++) {
+    if (visited[v] === false && dist[v] <= min) {
+      min = dist[v];
+      minIndex = v;
     }
-    return minIndex; //第一次调用得到结果为0
-  };
-   
-  const dijkstra = function(srcV) {
-    /**
-     * @param srcV: 源顶点的索引
-     */
-    const dist = [];//存放A分别到A、B、C、D、E、F的最短路径距离
-    const visited = [];//存放是否已经访问过A、B、C、D、E、F
-    const length = graphNew.length;
+  }
+  return minIndex; //第一次调用得到结果为0
+};
+  
+const dijkstra = function(srcV) {
+  /**
+   * @param srcV: 源顶点的索引
+   */
+  const dist = [];//存放A分别到A、B、C、D、E、F的最短路径距离
+  const visited = [];//存放是否已经访问过A、B、C、D、E、F
+  const length = graphNew.length;
 
-    for (let i = 0; i < length; i++) {
-      dist[i] = INF;//先把最短路径距离都初始化为无限大
-      visited[i] = false;
-    }
-
-    dist[srcV] = 0; //源顶点自己到自己的距离当然为0
-
-    for (let i = 0; i < length-1; i++) {
-      console.log(`dist:${dist.toString()}`);
-      console.log(`visited: ${visited.toString()}`);
-      const u = minDistance(dist, visited);//找出未访问顶点中距离源顶点最近的顶点的索引，第一次调用得到结果为0，即源顶点A自己
-      console.log(`u: ${u}`);
-      visited[u] = true;//下一轮循环再找下一个距离源顶点第二近的顶点时，就排除u了
-
-      for (let v = 0; v < length; v++) {//该循环可以找出u的所有相邻点v的dist[v]
-        if (!visited[v] && graphNew[u][v] != 0 && dist[u] != INF && dist[u] + graphNew[u][v] < dist[v]) {
-          //graphNew[u][v]是顶点u到顶点v的直接路径距离
-          //该条件可翻译为：如果顶点v没有被访问，且u到v的直接路径不为0， 且 源顶点到顶点u的最短路径不为INF, 且 (源顶点到顶点u的最短路径 与 顶点u到顶点v的直接路径) 距离之和 小于 源顶点到顶点v的最短路径距离
-          dist[v] = dist[u] + graphNew[u][v]; //更新源顶点到顶点v的最短路径的值
-          console.log(`v:${v}`);
-          console.log(`dist[v]: ${dist[v]}`);
-        }
-      }
-     
-    }
-    return dist;
+  for (let i = 0; i < length; i++) {
+    dist[i] = INF;//先把最短路径距离都初始化为无限大
+    visited[i] = false;
   }
 
-  console.log(dijkstra(0));
+  dist[srcV] = 0; //源顶点自己到自己的距离当然为0
+
+  for (let i = 0; i < length-1; i++) {
+    console.log(`dist:${dist.toString()}`);
+    console.log(`visited: ${visited.toString()}`);
+    const u = minDistance(dist, visited);//找出未访问顶点中距离源顶点最近的顶点的索引，第一次调用得到结果为0，即源顶点A自己
+    console.log(`u: ${u}`);
+    visited[u] = true;//下一轮循环再找下一个距离源顶点第二近的顶点时，就排除u了
+
+    for (let v = 0; v < length; v++) {//该循环可以找出u的所有相邻点v的dist[v]
+      if (!visited[v] && graphNew[u][v] != 0 && dist[u] != INF && dist[u] + graphNew[u][v] < dist[v]) {
+        //graphNew[u][v]是顶点u到顶点v的直接路径距离
+        //该条件可翻译为：如果顶点v没有被访问，且u到v的直接路径不为0， 且 源顶点到顶点u的最短路径不为INF, 且 (源顶点到顶点u的最短路径 与 顶点u到顶点v的直接路径) 距离之和 小于 源顶点到顶点v的最短路径距离
+        dist[v] = dist[u] + graphNew[u][v]; //更新源顶点到顶点v的最短路径的值
+        console.log(`v:${v}`);
+        console.log(`dist[v]: ${dist[v]}`);
+      }
+    }
+    
+  }
+  return dist;
+}
+
+console.log(dijkstra(0));
+
+///2.Floyd-Warshall算法：基于邻接矩阵
+const floydWarshall = function(graph) {
+  const dist = [];
+  const length = graph.length;
+  let i, j, k;
+
+  for (i = 0; i < length; i++) {
+    dist[i] = [];
+    for (j = 0; j < length; j++) {
+      if(i!==j && graph[i][j] === 0) { //邻接矩阵中为0的点之间距离直接设为INF
+        dist[i][j]=Infinity;
+      } else {
+        dist[i][j] = graph[i][j];
+      }
+    }
+  }
 
 
+  for (i = 0; i < length; i++) {
+    for (k = 0; k < length; k++) {
+      for (j = 0; j < length; j++) {
+        if (dist[i][k] + dist[k][j] < dist[i][j]) {
+          dist[i][j] = dist[i][k] + dist[k][j];
+        }
+      }
+    }
+  }
+  return dist;
+};
+console.log(floydWarshall(graphNew));
 
+//最小生成树
+///1.Prim算法
+const prim = function(graph) {
+  const parent = [];//存放每个顶点的parent顶点的index
+  const key = [];//存放每个顶点到parent顶点的距离
+  const visited = [];
+  const length = graph.length;//顶点个数
+  
+  for (let i = 0; i < length; i++) {
+    key[i] = INF;
+    visited[i] = false;
+  }//把所有顶点在最小生成树中到根顶点的距离初始化为INF,visited都初始化为false
 
+  key[0] = 0;//第一个顶点A作为最小生成树的根节点，那么它的key值就为0，parent顶点的序号为-1
+  parent[0] = -1;
+
+  for (let i = 0; i < length-1; i++) {//对每个顶点求最小生成树
+    let u = minDistance(key, visited);//先假设所有剩余顶点都是顶点u的子节点,key中存放着到顶点u的所有距离。当i=0时，即假设所有B,C,D,E,F都是A的子节点。搜索key数组的最小值，并返回它在key中的索引，即可以找到离A最近的那个点的索引。
+    visited[u] = true;//顶点u记为已被访问过
+    for (let v = 0; v < length; v++) {
+      if (graph[u][v] && visited[v] == false && graph[u][v] < key[v]) {//在未访问过的剩余顶点v中，找到u-v距离小于key[v]的那个顶点v，那么v的parent顶点就是u, key[v]就为u到v的距离
+        parent[v] = u;
+        key[v] = graph[u][v];
+      }
+    }
+
+  }
+  return parent;
+}
+console.log('Prim:');
+const graphMST = [ //A, B, C, D, E, F顶点的邻接矩阵
+  //A, B, C, D, E, F
+/*A*/[0, 2, 4, 0, 0, 0],//A->B距离为2， B->A距离为0
+/*B*/[2, 0, 2, 4, 2, 0],
+/*C*/[4, 2, 0, 0, 3, 0],
+/*D*/[0, 4, 0, 0, 3, 2],
+/*E*/[0, 2, 3, 3, 0, 2],
+/*F*/[0, 0, 0, 2, 2, 0]     
+];
+const parent = prim(graphMST);
+
+console.log(parent);//[-1, 0, 1, 5, 1, 4]
 
 export default Graph;
