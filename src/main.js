ad1a02c4c9b8bf8296049a6a17022cef540cacb3
wangyichen1064431086 <1064431086@qq.com>
2018-06-24 17:08:38 +0800

    Finish the reviewing of heap sort

--- src/main.js
+++ src/main.js
@@ -1,50 +1,52 @@
-function quickSort(arr) {
-  return quick(arr, 0, arr.length-1);
-}
-function quick(arr, left, right) {
-  if(arr.length>1) {
-    const sliceIndex = partition(arr, left, right);
-    console.log(`index:${sliceIndex}`);
-    if (left < sliceIndex-1) {
-      quick(arr, left, sliceIndex-1);
-    }
-    if (sliceIndex < right) {
-      quick(arr, sliceIndex, right);
-    }
-  }  
- return arr;这个返回不返回都行
-}
-function partition(arr, left, right) {
-  let i = left;
-  let j = right;
-  console.log(`arr:${arr}  left:${left} right:${right}`);
-  //const pivot = arr[Math.floor((i+j)/2)];
-  const pivot = arr[i];
-  console.log(`pivotIndex:${Math.floor((i+j)/2)}`);
-  console.log(`pivot:${pivot}`);
-  while(i<j) {
-    ///一开始肯定是i<j
-    while (arr[i] < pivot) {
-      i++;
-    }
-    while (arr[j] > pivot) {
-      j--;
-    }
-    ///这两步执行完，可能出现i==j或i<j
+function heapSort(arr) {
+  let len = arr.length;
+  if (len === 0) {
+    return;
+  }
+  buildHeap(arr);
 
-    if(i<j) { //这里是二者交换的条件，二者相等的时候就为同一项，所以不需要i<=j,只需要i<j
-      const temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-    }
-    if(i<=j){ //这里条件一定要是i<=j。 
-      //如果条件仅仅是i<j，那么当i==j的时候就永远也进入不了这个条件，return 的值就是i也是j。那么这一整段partion代码下来，i可能从来没有变过，又原封不动地return了i,即  const sliceIndex = partition(arr, left, right);的这个sliceIndex是和参数left相等的，那么在接下来的quick(arr, sliceIndex, right)就等于之前的quick(arr,left,right)，无限循环了
-      //总之，这个条件的目的就是让i一定要改变一次
-      i++;
-      j--;
+  while(len > arr.length-10) {
+    const temp = arr[len-1];
+    arr[len-1] = arr[0];
+    arr[0] = temp;
+
+    len--;
+    if(len > 1) {
+      heaplify(arr, len, 0);
     }
   }
-  return i;
 }
-const array = [3,5,1,6,4,4,7,2,2];
-console.log(quickSort(array));
+
+function buildHeap(arr) {
+  const len = arr.length;
+  const lastParentIndex = Math.floor(len/2) - 1;
+
+  for(let i=lastParentIndex; i>=0; i--) {
+    heaplify(arr, len, i);
+  }
+}
+
+function heaplify(arr, len, index) {
+  const leftIndex = index*2 + 1;
+  const rightIndex = index*2 + 2;
+  let biggestValueIndex = index;
+
+  if (leftIndex < len && arr[leftIndex] > arr[biggestValueIndex]) {
+    biggestValueIndex = leftIndex;
+  }
+
+  if (rightIndex < len && arr[rightIndex] > arr[biggestValueIndex]) {
+    biggestValueIndex = rightIndex;
+  }
+
+  if (biggestValueIndex !== index) {
+    const temp = arr[index];
+    arr[index] = arr[biggestValueIndex];
+    arr[biggestValueIndex] = temp;
+    heaplify(arr, len, biggestValueIndex);
+  }
+}
+
+const arr = [3, 5, 1, 6, 4, 7, 2, 9, 10, 13, 12, 14, 15, 16, 17];
+heapSort(arr);
+console.log(arr);
--- src/sort/heapSort.js
+++ src/sort/heapSort.js
@@ -1,33 +1,33 @@
 /*******堆排序算法实现************/
 function heapSort(arr) {
   let heapSize = arr.length;
-  buildHeap(arr);//把最大的那个节点换到了根节点
+  buildHeap(arr);//构造一个所有节点都满足arr[parent[i]] > arr[i]的堆结构数组，这样就把值最大的那个节点换到了根节点
   while(heapSize > 1) {
+    //在当前树中，交换位于根节点的最大值和最后一个节点的值,这样就把最大值排在了最后一个节点，这样就排好了最大值
     const temp = arr[0];
     arr[0]=arr[heapSize-1];
-    arr[heapSize-1] = temp;//在当前树中，交换位于根节点的最大值和最后一个节点的值,这样就排好了这个最大值
+    arr[heapSize-1] = temp;
     console.log('Exchange:')
     console.log(arr);
 
     heapSize--;//当前树中最后一个节点已经排好了值，故后面就不用再考虑这个节点，故新的树的大小减一
     if (heapSize>1) {
-      heapify(arr, heapSize, 0);//交换后新的根节点不满足它以下的子节点都有arr[parent[i]]<arr[i]，故要在新的树中对新的根节点再进行heapify处理
+      heapify(arr, heapSize, 0);//上面的交换操作产生了新的根节点，新的根节点只是通过跟最后一个节点交换得到了值，故新的根节点不满足其以下的子节点都有arr[parent[i]]<arr[i]，故要在新的树中对新的根节点再进行heaplify处理，直到满足从根节点往下的子节点都有arr[parent[i]]>=arr[i]
       console.log('heaplify:');
       console.log(arr);
-      
-
     }
-
   }
 }
 
+/**
+ * @description 构造一个所有节点都满足arr[parent[i]] > arr[i]的堆结构数组
+ * @param {Array} arr 待排序数组
+ */
 function buildHeap(arr) {
   const heapSize = arr.length;
-  const firstHeapifyIndex = Math.floor(heapSize/2-1);//从树的倒数第二层的最后一个有子节点的节点（对于满二叉树就是倒数第二层的最后一个节点）开始进行heapify处理
-  console.log('First build heap:');
+  const firstHeapifyIndex = Math.floor(heapSize/2-1);//从树的倒数第二层的最后一个有子节点的节点（对于满二叉树就是倒数第二层的最后一个节点）开始进行heapify处理。(heapSize/2-1)就是这个最后一个有子节点的节点索引。
   for (let i=firstHeapifyIndex; i >= 0; i--) {//从0到firstHeapifyIndex都要进行heapify处理，才能把最大的那个节点换到根节点
     heapify(arr, heapSize, i);
-    console.log(arr);
   }
 }
 
@@ -38,22 +38,24 @@ function buildHeap(arr) {
  * @param {*} i TYPE Number arr数组中、heapSize长度的树中的当前要进行往子节点替换的节点的索引
  */
 function heapify(arr, heapSize, i) {
-  const leftIndex = i * 2 + 1;
-  const rightIndex = i * 2 + 2;
+  const leftIndex = i * 2 + 1;//索引i的节点的左子节点索引
+  const rightIndex = i * 2 + 2;//索引i的节点的右子节点索引
   let biggestValueIndex = i;
   if (leftIndex < heapSize && arr[leftIndex] > arr[biggestValueIndex]) {
     //节点的最大index为heapSize-1
     //注意：这两次比较要跟arr[biggestValueIndex]比较，不能跟arr[i]比较，因为biggestValueIndex是会在左右i之间更新的
-    biggestValueIndex = leftIndex;
+    biggestValueIndex = leftIndex; //如果左子节点的值大于biggestValueIndex的值（此时就是根节点的值），那么更新biggestValueIndex为左子节点索引
   }
   if (rightIndex < heapSize && arr[rightIndex] > arr[biggestValueIndex]) {
-    biggestValueIndex = rightIndex;
+    biggestValueIndex = rightIndex;//如果右子节点的值大于biggestValueIndex的值(此时可能是根节点的值，也可能是左子节点的值)，那么更新biggestValueIndex为右子节点索引
   }
-  if (biggestValueIndex !== i) {
+  if (biggestValueIndex !== i) { //如果biggestValueIndex是左子节点索引或右子节点索引，那么交换根节点与biggestValueIndex节点的值
     const temp = arr[i];
     arr[i] = arr[biggestValueIndex];
     arr[biggestValueIndex] = temp;
-    heapify(arr, heapSize, biggestValueIndex);//要继续对biggestValueIndex进行往子节点交换处理,直到树的这个分支到叶子节点都满足arr[parent[i]]>=arr[i]
+
+    //交换后，被交换的那个子节点（左子节点或右子节点）往下可能就不再满足[parent[i]]>=arr[i]，所以要继续对biggestValueIndex进行heaify处理，即将biggestValueIndex可能需要和子节点进行值交换,直到树的这个分支到叶子节点都满足arr[parent[i]]>=arr[i]
+    heapify(arr, heapSize, biggestValueIndex);//要
 
   }
 }
